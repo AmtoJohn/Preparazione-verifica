b5454023c858f65036117c4ba7f8b09201db4f8d
AmtoJohn <amto.john@iisgalvanimi.edu.it>
2023-05-15 12:20:51 +0000

    Punto 2

--- Alimenti/src/app/app-routing.module.ts
+++ Alimenti/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { ObjectDetailsComponent } from './object-details/object-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'object/:id', component: ObjectDetailsComponent },
+  // Altre rotte se necessario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
